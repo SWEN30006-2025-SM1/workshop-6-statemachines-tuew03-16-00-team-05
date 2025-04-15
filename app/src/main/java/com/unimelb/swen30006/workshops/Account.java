3a3c4d04080a7796788de2f67196261ee4cffcb9
Saifullah <saifullahi@student.unimelb.edu.au>
2025-04-15 17:40:30 +1000

    Added method names to Account class

--- app/src/main/java/com/unimelb/swen30006/workshops/Account.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Account.java
@@ -1,5 +1,16 @@
 package com.unimelb.swen30006.workshops;
 
 public class Account {
-    public void onboard() {}
+    public void onboard() {
+
+    }
+    public void resumeAccount() {}
+    public void suspendAccount() {}
+    public void unsuspendAccount() {}
+    public void deactivateAccount() {}
+    public void closeAccount() {}
+    public void issueNewCard() {}
+    public void activateCard() {}
+    public void defaultAccount() {}
+
 }
